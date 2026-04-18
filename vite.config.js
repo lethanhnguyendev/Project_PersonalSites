9c6675eb2da534432ed5047d8654b851b667936d
lethanhnguyendev <nguyenlethanhdev@gmail.com>
2026-04-19 00:09:27 +0930

    Modify vite.config had ML project page

--- vite.config.js
+++ vite.config.js
@@ -18,6 +18,7 @@ export default defineConfig({
         'Portfolio/SmartRetail.html',
         'Portfolio/QRCodeScanMobile.html',
         'Portfolio/AdelaideMetroRealtime.html',
+        'Portfolio/MLSolarIssueDetection.html',
       ],
     },
   },
